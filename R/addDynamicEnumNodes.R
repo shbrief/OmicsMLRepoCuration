a004824c8650c90a253b5702e32f1c9586c1c17d
Sehyun Oh <shbrief@hotmail.com>
2024-03-11 22:32:08 -0400

    Functions from OmicsMLRepoR

---
+++ R/addDynamicEnumNodes.R
@@ -0,0 +1,42 @@
+#' Get the top nodes for dynamic enum.
+#'
+#' @import dplyr
+#' @importFrom OmicsMLRepoR get_ontologies
+#'
+#' @param curated_col A character (1). The curated column name to find
+#' the dynamic enum node for
+#' @param dd A data frame. Data dictionary including the `curated_col` under
+#' its `col.name` column.
+#'
+#' @export
+addDynamicEnumNodes <- function(curated_col, dd) {
+
+    ## Find the `curated_col` in the data dictionary
+    colnames <- c(curated_col,
+                  paste0("curated_", curated_col))
+    ind <- which(dd$col.name %in% colnames)
+
+    ## Stop if the `curated_col` doesn't exist in the data dictionary
+    if (length(ind) == 0) {
+        msg <- paste(curated_col, "doesn't exist in the provided data dictionary")
+        stop(msg)
+    }
+
+    ## Separate the ontoloty term ids and their ontologies
+    terms <- dd[[ind, "ontology"]] %>% strsplit(split = "\\|") %>%
+        unlist %>% na.omit %>% as.vector
+    onto <- OmicsMLRepoR::get_ontologies(terms = terms, delim = ":")
+
+    ## Remove SNOMED from the ontology term id
+    terms <- gsub("SNOMED:", "", terms)
+
+    ## Find the top nodes for dynamic enum
+    topNodes <- commonNodes(ids = terms, dbs = onto)
+    res <- paste0(topNodes$ontology_term_id, collapse = ";")
+
+    ## Add dynamic enum node(s) to the data dictionary
+    dd$dynamic_enum[ind] <- res
+    dd$dynamic_enum_property[ind] <- "descendent"
+
+    return(dd)
+}
---
+++ R/oxoMap.R
@@ -0,0 +1,54 @@
+#' Mapping terms to the same ontology
+#'
+#' This function maps input ontology terms to a identical/similar terms in
+#' the target ontology through Ontology Xref Service (OxO) API.
+#' (https://www.ebi.ac.uk/spot/oxo/)
+#'
+#' @importFrom httr2 request req_headers resp_body_json req_perform
+#'
+#' @param term_ids A character vector of term ids to retrieve mappings for
+#' @param target_ontology A character (1). The target ontology library to
+#' map the `term_ids` to.
+#' @param mapping_distance Optional. Mapping distance as an integer, 1
+#' (default), 2, or 3.
+#'
+#' @return A list of of data frames with mapping results for each input term
+#'
+#' @examples
+#' term_ids <- c("NCIT:C2855", "EFO:0000228", "NCIT:C35025", "HP:0003003",
+#'               "HP:0001824", "MONDO:0010200", "NCIT:C122328")
+#' oxoMap(term_ids, "NCIT")
+#'
+oxoMap <- function(term_ids, target_ontology, mapping_distance = 1) {
+  # Validate input
+  stopifnot(is.character(term_ids),
+            is.character(target_ontology),
+            is.numeric(mapping_distance),
+            mapping_distance %in% c(1, 2, 3))
+
+  # Prepare inputs
+  term_ids <- as.list(term_ids)
+  target_ontology <- as.list(target_ontology)
+
+  # Prepare request
+  req <- request("https://www.ebi.ac.uk/spot/oxo/api/search?size=200") %>%
+    req_headers("Content-Type" = "application/json",
+                "Accept" = "application/json") %>%
+    req_body_json(list(ids = term_ids,
+                       inputSource = NULL,
+                       mappingTarget = target_ontology,
+                       distance = mapping_distance))
+
+  # Perform request
+  resp <- req_perform(req)
+
+  # Parse response
+  resp_json <- resp_body_json(resp)
+  resp_list <- lapply(resp_json$`_embedded`$searchResults,
+                      function(x) as.data.frame(do.call(rbind,
+                                                        x$mappingResponseList)))
+  names(resp_list) <- term_ids
+
+  # Return as list of data frames
+  return(resp_list)
+}
