61ad10d8cf8ff8bba1bd6348264592c7d9a27985
Sehyun Oh <shbrief@gmail.com>
2026-01-26 10:07:58 -0500

    remove OmicsMLRepoR from dependency

--- R/addDynamicEnumNodes.R
+++ R/addDynamicEnumNodes.R
@@ -1,7 +1,31 @@
+#' Extract ontology from the ontology term ids
+#'
+#' @param terms A character vector
+#' @param delim A character. Delimiter between ontology and its id.
+#' Default is `:`.
+#' 
+#' @return A character vector containing the ontology names of the input 
+#' `terms`. The length of this is same as the `terms` input. 
+#'
+#' @examples
+#' terms <- c("HP:0001824", "MONDO:0010200", "NCIT:C122328", "4471000175100")
+#' .get_ontologies(terms = terms)
+#'
+.get_ontologies <- function(terms, delim = ":") {
+    
+    ontologies <- c()
+    for (i in seq_along(terms)) {
+        onto <- strsplit(terms[i], delim)[[1]][1]
+        isSNOMED <- letters_only(onto)
+        if (isFALSE(isSNOMED)) {onto <- "SNOMED"}
+        ontologies[i] <- onto
+    }
+    return(ontologies)
+}
+
 #' Get the top nodes for dynamic enum.
 #'
 #' @import dplyr
-#' @importFrom OmicsMLRepoR get_ontologies
 #' @importFrom stats na.omit
 #'
 #' @param curated_col A character (1). The curated column name to find
@@ -26,7 +50,7 @@ addDynamicEnumNodes <- function(curated_col, dd) {
     ## Separate the ontology term ids and their ontologies
     terms <- dd[[ind, "ontology"]] %>% strsplit(split = "\\|") %>%
         unlist %>% stats::na.omit %>% as.vector
-    onto <- OmicsMLRepoR::get_ontologies(terms = terms, delim = ":")
+    onto <- .get_ontologies(terms = terms, delim = ":")
 
     ## Remove SNOMED from the ontology term id
     terms <- gsub("SNOMED:", "", terms)
